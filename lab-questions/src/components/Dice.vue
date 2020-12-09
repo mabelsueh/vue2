d838a67b7b139debccdc89f99ed752990e3a4961
mabelsueh <su.eh.mabel@gmail.com>
2020-12-09 09:43:57 +0000

    added missing !

---
+++ lab-questions/src/components/Dice.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="container dice">
+      {{ number }}<span class="even" v-if="number%2==0">!</span>
+    </div>
+    <br>
+    <button @click="randomize">Roll Dice</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return { number: 1 };
+  },
+  methods: {
+    randomize: function () {
+      console.log((this.number = Math.floor(Math.random() * 6) + 1));
+    },
+  },
+};
+</script>
+
+<style scoped>
+.dice {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: red;
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+}
+.even {
+    color: black;
+}
+</style>
