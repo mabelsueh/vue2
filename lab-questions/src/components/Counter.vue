996f38aeda25044cd531187e3c88360a82c52915
mabelsueh <su.eh.mabel@gmail.com>
2020-12-10 03:26:58 +0000

    finished lab 3.1

--- lab-questions/src/components/Counter.vue
+++ lab-questions/src/components/Counter.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
-    <div class="container counter">
-      {{ count }}
-    </div>
-    <div>
-      <span v-if="count % 2 == 0" style="margin-left: 10px">Even</span>
+    <div >
+       <div class="container counter">
+        <div v-if="count%2==0">
+            <h1 v-bind:style="{'color':'green'}">{{count}}</h1>
+       </div>
+
+         <div v-else>
+            <h1 v-bind:style="{'color':'red'}">{{count}}</h1>
+       </div>
+       </div>
+
+    <!-- <div>
+      for the text only, not numbers (lab 2.1)
+      <span v-if="count % 2 == 0" style="margin-left: 10px">{{count}}</span>
       <span v-else style="margin-left: 10px">Odd</span>
-    </div>
+    </div> -->
+    <div>
     <button @click="increase">Increase by 1</button>
     <button @click="decrease">Decrease by 1</button>
-  </div>
+    </div>
+
+    </div>
 </template>
 
 <script>
@@ -17,6 +28,7 @@ export default {
   data: function () {
     return { count: 0 };
   },
+  //   for the buttons only
   methods: {
     increase: function () {
       this.count += 1;
