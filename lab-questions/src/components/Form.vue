4b0912efe3aadf7f1c4d473991b243f97755d7a6
mabelsueh <su.eh.mabel@gmail.com>
2020-12-10 03:31:06 +0000

    added form boilerplate

---
+++ lab-questions/src/components/Form.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <form>
+            <label>Name</label>
+            <input type="text"/>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+
+        }
+    }
+}
+</script>
